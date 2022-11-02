0d3e9f0d6ebf6187f59fd550e226379f0f2cd21f
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-02 13:49:40 -0600

    added quiz examples for factorial and while-as-a-for loop

---
+++ factorial.js
@@ -0,0 +1,20 @@
+// let solution = 1;
+
+// for (let x = 0; x <= 12; x++){
+//   for (let y = x; y > 0; y--){
+//     solution *= y;
+//   }
+//   console.log(solution); //check final solution
+//   solution = 1;
+// }
+
+// I went a bit more complex than was necessary because I didn't read all the instructions
+
+let solution = 1;
+
+for (let i = 1; i <= 12; i++) {
+  solution *= i;
+}
+
+console.log(solution);
+
---
+++ whileasfor.js
@@ -0,0 +1,9 @@
+var y = 9;
+while (y >= 1) {
+  console.log("hello " + y);
+  y = y -1;
+}
+
+for (let x = 9; x >= 1; x -= 1) {
+  console.log("hello " + x);
+}
