bc7ea8e01a8b4ac76a183c6e57c9b1deb59cc847
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-02 11:38:53 -0600

    updated whileloop with incrementing example and added fizzbuzz quiz example

--- whileloop.js
+++ whileloop.js
@@ -3,3 +3,15 @@ while (x <= 10000) {
   console.log(x+" mississippi");
   x += 1;
 }
+
+let start = 0; //when to start
+while (start < 10) { // when to stop
+  console.log(start);
+  start = start + 2; // how to get to the next item
+}
+
+let y = 10;
+while (y <= 25) {
+  console.log('Printing out x = ' + y);
+  y = y + 2;
+}
