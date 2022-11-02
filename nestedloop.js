a8009ef95d777b24528e0a140a5f80c4e679d877
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-02 12:21:08 -0600

    added simple loop example for quiz

---
+++ nestedloop.js
@@ -0,0 +1,11 @@
+for (var x = 0; x < 5; x += 1) { // outer loop
+  for (var y = 0; y < 3; y += 1) { // inner loop
+    console.log(x + "," + y);
+  }
+}
+
+//Quiz question
+
+for (var i = 0; i <= 6; i = i + 2) {
+  console.log(i);
+}
