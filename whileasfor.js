e9ecc38bd745ca16c5739e54e8158c67a45661e0
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-07 15:29:06 -0700

    change file name of looping-lastIndexOf.js

--- whileasfor.js
+++ whileasfor.js
@@ -1,8 +1,8 @@
-var y = 9;
-while (y >= 1) {
-  console.log("hello " + y);
-  y = y -1;
-}
+//var y = 9;
+//while (y >= 1) {
+//  console.log("hello " + y);
+//  y = y -1;
+//}
 
 for (let x = 9; x >= 1; x -= 1) {
   console.log("hello " + x);
