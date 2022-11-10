fdd72e4932c3139907e87478d1736fbd8cf57df1
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-10 11:29:32 -0700

    cleaned up and added comments for kata21

--- stretch/kata21-number-guesser.js
+++ stretch/kata21-number-guesser.js
@@ -1,6 +1,3 @@
-// Reading input from the command line
-// -- Reading input from the command line in node is quite difficult in a normal situation. 
-// -- For this kata we'd like you to setup a package.json file along side your code for this kata:
 let prompt = require("prompt-sync")(
   {
     "dependencies": {
@@ -8,27 +5,26 @@ let prompt = require("prompt-sync")(
     }
   }
 );
-// code below (replace this example)
 
 const randomNumber = Math.floor(Math.random() * (100) + 1); // generate a random number between 1 and 100 (inclusive)
 let answer;
 let attempts = [];
 
 const numberGuesser = function() {
-  do { //do...while loop so it runs at least once
+  do { // do...while loop so it runs at least once
     answer = prompt("Guess a number: ");
-    if (Number(answer) === randomNumber) {
+    if (Number(answer) === randomNumber) { //i nput is as string so need to convert to a number
       attempts.push(Number(answer));
       return console.log("You got it! You took " + (attempts.length) + " attempts!");
     } else if (Number(answer) < randomNumber) {
-        if (attempts.includes(Number(answer))) {
+        if (attempts.includes(Number(answer))) { // not counted as an attempt if it was already attempted
           console.log("Already Guessed!");
         } else {
           attempts.push(Number(answer));
           console.log("Too Low!");
         }
     } else if (Number(answer) > randomNumber) {
-        if (attempts.includes(Number(answer))) {
+        if (attempts.includes(Number(answer))) { // not counted as an attempt if it was already attempted
           console.log("Already Guessed!");
         } else {
           attempts.push(Number(answer));
@@ -37,7 +33,7 @@ const numberGuesser = function() {
     } else if ((typeof answer) !== "number") { // not counted as an attempt as it is not a number
         console.log("Not a number! Try again!");
     }        
-  } while (answer !== randomNumber); //keeps running until the number inputted by user matches the randomly generated number
+  } while (answer !== randomNumber); // keeps running until the number inputted by user matches the randomly generated number
 };
 
 numberGuesser();
